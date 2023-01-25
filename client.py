23babe323df9fe2662673dc84f50065f4ffce32e
lurbaby <120603922+lurbaby@users.noreply.github.com>
2023-01-25 15:49:37 +0200

    update

--- client.py
+++ client.py
@@ -1,40 +1,104 @@
-import socket,os
+import socket, os
+from time import sleep
 
-ip,port = 'localhost',1235
+# constants
+
+ip,port = 'localhost',9999
 FORMAT = 'utf-8'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((ip, port))
+disk_path = ['A:','B:','C:','E:','F:','D:']
 
+try:
+	a = client.connect((ip, port))
+	print(f'[CONNECT] to {ip}:{port}')
+except:
+	print('[ERROR] server is turned off')
+	sleep(0.44444)
+	exit()
+# starting function
 
 def file_info():
-	try:
-		file_name = input('file name: ')
-		if 'disconnect' in file_name:
-			client.close()
+	
+	file_name = input('file name or path : ').replace('"','')
+	file_name_send = None
+	if file_name == '-l':
+		return 1
+	for starts_ in disk_path:
+		if file_name.startswith(starts_):
+			file_name_send = file_name.split('\\')
+			file_name_send = [i for i in file_name_send if '.' in i[-5:-1]]
+			file_name_send = file_name_send[0].replace('"','')
+			break
+	else:
+		file_name_send = file_name
 
+	try:
 		file_size = os.path.getsize(file_name)
-		client.send(f'{file_name}+{file_size}'.encode(FORMAT))
-		print(f'[SENDING] file : {file_name}')
+
+		client.send(f'{file_name_send}+{file_size}'.encode(FORMAT))
+	
+		print(f'[SENDING] file name: {file_name}')
+		sleep(0.4)
+
 		return file_name
 	except:
-		print('[ERROR] file not found')
+		return 2
+
+def sender_(file_name):
+
+	with open(file_name,'rb') as bin_:
+		binary_read = bin_.read(2048)
+
+		while binary_read:
+			client.send(binary_read)
+			binary_read = bin_.read(2048)
+		return True
 
 
+def receiver_(user, address):
+	while True:
 
+		print('[MODE] receiver mode activate')
+		try:
+			file_name = (user.recv(2048).decode(FORMAT)).split('+')
+			print(f'name: {file_name[0]} | size: {file_name[1]}')
+
+			with open('received_' + file_name[0], 'wb') as bin_:
+				bytes_recv = 0
+				while bytes_recv != int(file_name[1]):
+					data = user.recv(2048)
+					bin_.write(data)
+					bytes_recv += len(bytes(data))
+			sleep(0.5)
+			return True
+		except:
+			print('[ERROR] connecting error' + '\n')
+			return False
+
+mode = int(input('[MODE] select mode'))
 while True:
-
-	try:
-		file_name = file_info()
-
-		file = open(file_name,'rb')
-		data = file.read(2048)
-
-		while data:
-			client.send(data)
-			data = file.read(2048)
-
-		file.close()
-	except:
-		print('[ERROR] file not found')
-
-
+	if mode == 1:
+		print('[MODE] sender mode activate\n')
+		try:
+			file_name = file_info()
+			if file_name == 1:
+				print('[CLOSING] closing connection')
+				sleep(2)
+				break
+			elif file_name == 2:
+				print('[ERROR] file not found')
+				sleep(0.44)
+				continue
+			try:
+				send_flag = sender_(file_name)
+				if not send_flag:
+					print('[ERROR] file not found')
+					continue
+			except:
+				print('[ERROR] while sending file')
+				continue
+		except:
+			print('[ERROR] something went wrong ')
+	elif mode == 2:
+		while True:
+			pass
